f73bf964b87662437706595b5d69a42296a72361
Vrushalikokare67 <86995729+Vrushalikokare67@users.noreply.github.com>
2021-11-29 16:40:47 +0530

    Create outputfibonacci1.c

---
+++ nth_factorial/outputfibonacci1.c
@@ -0,0 +1,10 @@
+Enter the nth number in fibonacci series: 5
+The 5 number in fibonacci series is 10
+
+
+Enter the nth number in fibonacci series: 8
+The 8 number in fibonacci series is 21
+ 
+
+Enter the nth number in fibonacci series: 12
+The 12 number in fibonacci series is 144
